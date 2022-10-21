945cdc9e4d5951f567c017989390770c80ea5511
Gustave Monce <gustave.monce@outlook.com>
2022-10-21 18:57:07 +0200

    Drivers: Various Updates
    This update does not include anything new and is designed to test our servicing pipeline

--- tools/pack-release.cmd
+++ tools/pack-release.cmd
@@ -8,6 +8,7 @@ mkdir ..\..\SurfaceDuo-Drivers-Release
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\SurfaceDuo-Drivers-Release\PLATFORM.SOC_QC8150.BASE.zip ..\components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\SurfaceDuo-Drivers-Release\PLATFORM.SOC_QC8150.BASE_MINIMAL.zip ..\components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE_MINIMAL
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\SurfaceDuo-Drivers-Release\DEVICE.SOC_QC8350.ZETA.zip ..\components\QC8350\Device\DEVICE.SOC_QC8350.ZETA
+"%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\SurfaceDuo-Drivers-Release\DEVICE.SOC_QC8350.ZETA_MINIMAL.zip ..\components\QC8350\Device\DEVICE.SOC_QC8350.ZETA_MINIMAL
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\SurfaceDuo-Drivers-Release\PLATFORM.SOC_QC8350.BASE.zip ..\components\QC8350\Platform\PLATFORM.SOC_QC8350.BASE
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\SurfaceDuo-Drivers-Release\PLATFORM.SOC_QC8350.BASE_MINIMAL.zip ..\components\QC8350\Platform\PLATFORM.SOC_QC8350.BASE_MINIMAL
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\SurfaceDuo-Drivers-Release\SUPPORT.DESKTOP.BASE.zip ..\components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.BASE
