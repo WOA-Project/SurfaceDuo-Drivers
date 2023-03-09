9bec9f9b2779a0fd48cd889f7c8f9473b22080a2
Gustave Monce <gustave.monce@outlook.com>
2023-03-09 22:43:25 +0100

    AdaptationKits: ¯\_ (ツ)_/¯

---
+++ adaptationkits/AOS/patch.epsilon.cmd
@@ -0,0 +1,38 @@
+@echo off
+set EFIESP=I:
+set MainOS=J:
+set VIRT_EFIESP=K:
+
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablebootdebugpolicy   No
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablebootorderclean    No
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enabledeviceid          No
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablemassstorage       No
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablerpmbprovisioning  No
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablesecurebootpolicy  No
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablestartcharge       No
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enableresettpm          No
+
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {default} nointegritychecks       Yes
+bcdedit /store %EFIESP%\efi\Microsoft\Boot\BCD /set {default} testsigning             Yes
+
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablebootdebugpolicy   No
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablebootorderclean    No
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enabledeviceid          No
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablemassstorage       No
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablerpmbprovisioning  No
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablesecurebootpolicy  No
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enablestartcharge       No
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {01de5a27-8705-40db-bad6-96fa5187d4a6} enableresettpm          No
+
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {default} nointegritychecks       Yes
+bcdedit /store %VIRT_EFIESP%\efi\Microsoft\Boot\BCD /set {default} testsigning             Yes
+
+ren %EFIESP%\efi\Microsoft\Boot\SecureBootPolicy.p7b SecureBootPolicy.p7b.bak
+
+ren %VIRT_EFIESP%\efi\Microsoft\Boot\SecureBootPolicy.p7b SecureBootPolicy.p7b.bak
+
+move %MainOS%\Windows\System32\EnableWcosUefiSbTest.exe %MainOS%\Windows\System32\EnableWcosUefiSbTest.disabled.exe
+
+reg load HKLM\RTS %MainOS%\Windows\System32\config\SYSTEM
+reg add HKLM\RTS\ControlSet001\Control\Bitlocker /v PreventDeviceEncryption /t REG_DWORD /d 1 /f
+reg unload HKLM\RTS
