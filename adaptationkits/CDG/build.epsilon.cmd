c7174e885b2130c1124ceb1b2c4d151f9e4e2653
Gustave Monce <gustave.monce@outlook.com>
2023-04-16 23:42:38 +0200

    Epsilon: CDG Integration Work for Surface Duo 1 based devices, version 2304.1

--- adaptationkits/CDG/build.epsilon.cmd
+++ adaptationkits/CDG/build.epsilon.cmd
@@ -1,15 +1,4 @@
-@echo off
-
-      SET HOUR=%time:~0,2%
-      SET dtStamp9=%date:~-2%%date:~4,2%%date:~7,2%-0%time:~1,1%%time:~3,2%
-      SET dtStamp24=%date:~-2%%date:~4,2%%date:~7,2%-%time:~0,2%%time:~3,2%
-
-      if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
-
-      set SIGN_OEM=1
-      set DIR=%CD%
-      cd /d "C:\Program Files (x86)\Windows Kits\10\tools\bin\i386"
-
-      mkdir "%DIR%\Image_%dtStamp%_Epsilon"
-      imggen "%DIR%\Image_%dtStamp%_Epsilon\Flash.vhdx" "%DIR%\OEMInput.Epsilon.xml" "%DIR%" ARM64
-      cd %DIR%
+set _NTTREE=E:
+cd /d "C:\Program Files (x86)\Windows Kits\10\tools\bin\i386\"
+call imggen.cmd E:\Output\Flash.vhdx E:\OEMInput.xml E: ARM64
+cd /d E:
